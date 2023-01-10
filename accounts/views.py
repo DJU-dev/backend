8709bd15f607fbe8688d767c58dc9b5b054831bd
dawon <tomatomato2121@gmail.com>
2023-01-10 17:15:29 +0900

    auth settings, test

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework import serializers
 
-# Create your views here.
+User = get_user_model()
+
+class SignupSerialzer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class SignupView(CreateAPIView):
+    model = User
+    serializer_class = SignupSerialzer
+    permission_classes = [
+        AllowAny
+    ]
--- backend/settings.py
+++ backend/settings.py
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     # third apps
     'rest_framework',
     "corsheaders",
+    'rest_framework_simplejwt.token_blacklist',
+    'dj_rest_auth',
+    'dj_rest_auth.registration',
+    'allauth',
+    'allauth.account',
     # local apps
     'accounts',
 ]
@@ -133,6 +138,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# AUTH_USER_MODEL='accounts.User'
+
+
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
