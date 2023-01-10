57aa947f1c0c917387c2a51090d8fd83516444b9
dawon <tomatomato2121@gmail.com>
2023-01-11 02:16:09 +0900

    set rest auth url

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.SignupView.as_view(), name='signup'),
+    path('', include('dj_rest_auth.urls')),
+    path('registration/', include('dj_rest_auth.registration.urls')),
+    path('', include('allauth.urls')),
 ]
