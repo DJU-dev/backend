9ec10c458a5d555043a4989278ced4deab9df6f4
dawon <tomatomato2121@gmail.com>
2023-01-11 23:25:01 +0900

    fix

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('signup/', views.SignupView.as_view(), name='signup'),
     path('', include('dj_rest_auth.urls')),
-    path('registration/', include('dj_rest_auth.registration.urls')),
     path('', include('allauth.urls')),
+    path('registration/', include('dj_rest_auth.registration.urls')),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,1 @@
-from django.contrib.auth import get_user_model
-from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import AllowAny
-from rest_framework import serializers
 
-User = get_user_model()
-
-class SignupSerialzer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
-
-
-class SignupView(CreateAPIView):
-    model = User
-    serializer_class = SignupSerialzer
-    permission_classes = [
-        AllowAny
-    ]
