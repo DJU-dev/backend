349008f0c589118a84f897a686b40d7f990dcca6
dawon <tomatomato2121@gmail.com>
2023-01-14 04:29:18 +0900

    post models migrate

--- dslr/models.py
+++ dslr/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+
+class Post(BaseModel):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='dslr/post/%Y/%m/%d')
+    caption = models.CharField(max_length=500)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
