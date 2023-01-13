e5383fc2c7e97cb20179a6adb2d1a2a64555dcc5
dawon <tomatomato2121@gmail.com>
2023-01-14 06:43:26 +0900

    user information in the post

--- dslr/serializers.py
+++ dslr/serializers.py
@@ -1,8 +1,15 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from dslr.models import Post
 
+class AutherSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'username', 'email']
+
 
 class PostSerializers(serializers.ModelSerializer):
+    author = AutherSerializers(read_only=True)
     class Meta:
         model = Post
         fields = '__all__'
