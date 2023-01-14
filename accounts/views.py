55308de823aa6bc32bcf4e6c65127d4dcb909851
dawon <tomatomato2121@gmail.com>
2023-01-14 23:51:11 +0900

    filter using pk

--- accounts/views.py
+++ accounts/views.py
@@ -3,13 +3,16 @@ from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
 from accounts.serializers import ProfileSerializer
 from rest_framework.response import Response
+from dslr.models import Post
 
 User = get_user_model()
 class ProfileView(APIView):
-    def get(self, request):
-        user = get_object_or_404(User, pk=request.user.pk)
+    def get(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
         serializer = ProfileSerializer(user)
-        print(serializer.data)
-        return Response(serializer.data)
+        post_count = Post.objects.filter(author_id=pk).count()
+        new_serializer = {'post_count': post_count}
+        new_serializer.update(serializer.data)
+        return Response(new_serializer)
 
 
