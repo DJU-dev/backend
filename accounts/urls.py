3a29077bc2c0e9285de2e1ba68f7a00a81bd877d
dawon <tomatomato2121@gmail.com>
2023-01-14 22:59:18 +0900

    default profile API

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('', include('allauth.urls')),
     path('registration/', include('dj_rest_auth.registration.urls')),
-
+    path('profile/', views.ProfileView.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,15 @@
+from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from accounts.serializers import ProfileSerializer
+from rest_framework.response import Response
+
+User = get_user_model()
+class ProfileView(APIView):
+    def get(self, request):
+        user = get_object_or_404(User, pk=request.user.pk)
+        serializer = ProfileSerializer(user)
+        print(serializer.data)
+        return Response(serializer.data)
+
 
