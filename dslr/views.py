ddb41b9bb24feafc74cd3134b38367adad24d19e
dawon <tomatomato2121@gmail.com>
2023-01-14 22:38:19 +0900

    typo serializer

--- dslr/views.py
+++ dslr/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 from dslr.models import Post
-from dslr.serializers import PostSerializers
+from dslr.serializers import PostSerializer
 
 class ReadOnlyPostViewSet(ReadOnlyModelViewSet):
     queryset = Post.objects.all()
-    serializer_class = PostSerializers
+    serializer_class = PostSerializer
     permission_classes = [AllowAny]
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
-    serializer_class = PostSerializers
+    serializer_class = PostSerializer
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
