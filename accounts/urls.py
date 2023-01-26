c245fdf4259f5b1c4225ba682db5d4e4a20dce6c
dawon <tomatomato2121@gmail.com>
2023-01-27 08:02:16 +0900

    profile view

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', include('allauth.urls')),
     path('registration/', include('dj_rest_auth.registration.urls')),
     path('profile/<int:pk>/', views.ProfileView.as_view()),
+    path('my/profile/', views.MyProfileView.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -16,3 +16,13 @@ class ProfileView(APIView):
         return Response(new_serializer)
 
 
+class MyProfileView(APIView):
+    def get(self, request):
+        user = get_object_or_404(User, pk=request.user.pk)
+        serializer = ProfileSerializer(user)
+        post_count = Post.objects.filter(author=request.user).count()
+        new_serializer = {'post_count': post_count}
+        new_serializer.update(serializer.data)
+        return Response(new_serializer)
+
+
